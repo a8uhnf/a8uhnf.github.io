f6f134981a3e0ee0d4ce0269bc7562abcc4ecfab
a8uhnf <a8u.han1fa@gmail.com>
2018-05-12 04:43:58 +0600

    added-all

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm -rf {404.html,categories,content,css,fonts,img,index.html,index.xml,js,post,sass,sitemap.xml,tags}
+
+
+
+cp -r public/{404.html,categories,content,css,fonts,img,index.html,index.xml,js,post,sass,sitemap.xml,tags} .
+
